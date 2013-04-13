34e0bad39408e1b134e6a682297efb36cf16e97d
Nathan Wong <nwong212@gmail.com>
2013-04-13 15:28:59 -0700

    changed footer styles

--- footer.php
+++ footer.php
@@ -2,112 +2,116 @@
 <div class="clear"></div>
 </div>
 <div class="push">
-	<div class="footercontainer">
-<div id="footer">
-	<div class="footershadow"></div>
-<div class="footer-container">
-	<div class="footer-right">
-		<ul class="s">
-			<li class="header-responsive-hidden">
-				<h3>
-					Titanium Robotics
-				</h3>
-			<li>
-				<a href="/about-us">About Us</a>
-			</li>
-			<li>
-				<a href="/our-mission">Our Mission</a>
-			</li>
-			<li>
-				<a href="/contact-us">Contact Us</a>
-			</li>
-			<li>
-				<a href="/schedule">Schedule</a></li>
-			<li>
-				<a href="/alumni">Alumni</a>
-			</li>
-			<li>
-				<a href="/about-first">About <i>FIRST&reg;</i></a>
-			</li>
-			<li>
-				<a href="/team-leadership">Team Leadership</a>
-			</li>
-		</ul>
-		<ul class="h">
-			<li>
-				<h3 class="header-responsive-hidden">
-					Sub-Teams
-				</h3>
-			</li>
-			<li>
-				<a href="/resources/business">Business Team</a>
-			</li>
-			<li>
-				<a href="/resources/mechanical">Mechanical Team</a>
-			</li>
-			<li>
-				<a href="/resources/programming">Programming Team</a>
-			</li>
-			<li>
-				<a href="/resources/cad">CAD Team</a>
-			</li>
-			<li>
-				<a href="/resources/electrical">Electrical Team</a>
-			</li>
-			<li>
-				<a href="/resources/scouting">Scouting Team</a>
-			</li>
-			<li>
-				<a href="/resources/the-website">Website Team</a>
-			</li>
-		</ul>
-		<ul class="h">
-			<li>
-				<h3 class="header-responsive-hidden">
-					Media
-				</h3>
-			</li>
-			<li>
-				<a href="/media">Press</a>
-			</li>
-			<li>
-				<a href="/branding">Styles Guide</a>
-			</li>
-			<li>
-				<a href="/media/videos">Videos</a>
-			</li>
-			<li>
-				<a href="/media/photos">Photos</a>
-			</li>
-		</ul>
-		<ul class="h">
-			<li>
-				<h3 class="header-responsive-hidden">
-					Search
-				</h3>
-			</li>
-			<li>
-				<br />
-				<?php get_search_form( ); ?>
-			</li>
-		</ul>
-	</div>
-	<div class="bottom">
+	<div class="footercontainer"></div>
+		
+	<div id="footer">
+		<div class="footershadow"></div>
+		
+		<div class="footer-container">
+			
+			<div class="footer-right">
+				<ul class="s">
+					<li class="header-responsive-hidden">
+						<h3>
+							Titanium Robotics
+						</h3>
+					<li>
+						<a href="/about-us">About Us</a>
+					</li>
+					<li>
+						<a href="/our-mission">Our Mission</a>
+					</li>
+					<li>
+						<a href="/contact-us">Contact Us</a>
+					</li>
+					<li>
+						<a href="/schedule">Schedule</a></li>
+					<li>
+						<a href="/alumni">Alumni</a>
+					</li>
+					<li>
+						<a href="/about-first">About <i>FIRST&reg;</i></a>
+					</li>
+					<li>
+						<a href="/team-leadership">Team Leadership</a>
+					</li>
+				</ul>
+				<ul class="h">
+					<li>
+						<h3 class="header-responsive-hidden">
+							Sub-Teams
+						</h3>
+					</li>
+					<li>
+						<a href="/resources/business">Business Team</a>
+					</li>
+					<li>
+						<a href="/resources/mechanical">Mechanical Team</a>
+					</li>
+					<li>
+						<a href="/resources/programming">Programming Team</a>
+					</li>
+					<li>
+						<a href="/resources/cad">CAD Team</a>
+					</li>
+					<li>
+						<a href="/resources/electrical">Electrical Team</a>
+					</li>
+					<li>
+						<a href="/resources/scouting">Scouting Team</a>
+					</li>
+					<li>
+						<a href="/resources/the-website">Website Team</a>
+					</li>
+				</ul>
+				<ul class="h">
+					<li>
+						<h3 class="header-responsive-hidden">
+							Media
+						</h3>
+					</li>
+					<li>
+						<a href="/media">Press</a>
+					</li>
+					<li>
+						<a href="/branding">Styles Guide</a>
+					</li>
+					<li>
+						<a href="/media/videos">Videos</a>
+					</li>
+					<li>
+						<a href="/media/photos">Photos</a>
+					</li>
+				</ul>
+				<ul class="h">
+					<li>
+						<h3 class="header-responsive-hidden">
+							Search
+						</h3>
+					</li>
+					<li>
+						<br />
+						<?php get_search_form( ); ?>
+					</li>
+				</ul>
+			</div>
 
-		<a href="http://www.facebook.com/team1160" target="_blank">facebook/team1160</a> // <a href="http://twitter.com/frc1160" target="_blank">@frc1160</a>
-		<br />
-		<a href="/resources/the-website" target="_blank">About the Website</a> // <a href="https://github.com/nwong212/Team1160" target="_blank">Download Theme from GitHub</a>
-		<br />
-		<a href="http://www.usfirst.org" target="_blank"><img class="center" src="<?php bloginfo('template_directory'); ?>/assets/FIRST.png" width="50" alt="FIRST Logo; Visit FIRST" title="Visit FIRST" /></a>
-		<br />
-		Valid <a href="http://validator.w3.org/check?uri=<?php echo get_home_url(); ?>" target="_blank">HTML5</a>
-		<div class="copy">
-			&copy;<?php echo date(Y)?>&nbsp;<a href="<?php echo get_settings('home'); ?>"><?php bloginfo('name'); ?></a>  //  All rights reserved
+			<div class="bottom">
+
+				<a href="http://www.facebook.com/team1160" target="_blank">facebook/team1160</a> // <a href="http://twitter.com/frc1160" target="_blank">@frc1160</a>
+				<br />
+				<a href="/resources/the-website" target="_blank">About the Website</a> // <a href="https://github.com/nwong212/Team1160" target="_blank">Download Theme from GitHub</a>
+				<br />
+				<a href="http://www.usfirst.org" target="_blank"><img class="center" src="<?php bloginfo('template_directory'); ?>/assets/FIRST.png" width="50" alt="FIRST Logo; Visit FIRST" title="Visit FIRST" /></a>
+				<br />
+				Valid <a href="http://validator.w3.org/check?uri=<?php echo get_home_url(); ?>" target="_blank">HTML5</a>
+				<div class="copy">
+					&copy;<?php echo date(Y)?>&nbsp;<a href="<?php echo get_settings('home'); ?>"><?php bloginfo('name'); ?></a>  //  All rights reserved
+				</div>
+			</div>
+
 		</div>
 	</div>
-</div>
-</div>
-</div>
 </div>
 <?php wp_footer(); ?>
 <script type="text/javascript">
